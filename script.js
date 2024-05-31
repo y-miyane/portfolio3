bab10c6d8360560467fbda5449adb7ee2fc87444
y-miyane <y.miyane926@gmail.com>
2024-05-31 22:59:36 +0900

    5/31

--- script.js
+++ script.js
@@ -295,10 +295,11 @@ $(document).ready(function() {
 });
 
 
+// スムーススクロール //
 $(function(){
     $('a[href^="#"]').click(function(){
       var adjust = 0;
-      var speed = 400;
+      var speed = 1000;
       var href= $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top + adjust;
